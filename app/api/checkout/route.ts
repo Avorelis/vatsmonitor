1d960601336b3062e779eba4578892d58d5da684
Avorelis <patrick.karban92@gmail.com>
2025-05-03 02:14:53 +0200

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,20 +1,19 @@
-import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
+import { NextRequest, NextResponse } from 'next/server';
 
-const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY')!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
 });
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const { priceId } = await req.json();
 
   const session = await stripe.checkout.sessions.create({
     mode: 'subscription',
     line_items: [{ price: priceId, quantity: 1 }],
-    success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/checkout/success`,
-    cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/checkout/cancel`,
-    billing_address_collection: 'auto',
     subscription_data: { trial_period_days: 14 },
+    success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/checkout/success`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
   });
 
   return NextResponse.json({ url: session.url });
